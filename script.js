880d6c163c2941ee6b17b00c897f421e976aae6d
Janessa Groves <jgroves1@asu.edu>
2024-05-07 14:39:22 -0500

    Hero Slideshow
    created graphics for hero slides and began the javascript for slideshow

--- script.js
+++ script.js
@@ -1,13 +1,40 @@
 'use strict';
 
 // hero slideshow
+    // acquire elements
+    const heroSlides = document.querySelectorAll(".slide");
+    // NEED TO ADD LIL DOTS TO NAVIGATE BTWN IMAGES
+
+    let activeSlide = 0;
+        
+    // functions
+        function currSlide(n) {
+            heroSlides.forEach(slide => {
+                slide.classList.remove("active");
+            });
+
+            heroSlides[n].classList.add("active");
+        }
+
+        function nextSlide() {
+            activeSlide++;
+
+            if (activeSlide >= heroSlides.length) {
+                activeSlide = 0;
+            }
+
+            currSlide(activeSlide);
+        }
+
+    // automate it
+    setInterval(nextSlide, 5000);
 
 // today's hours
-const today = new Date();
-let day =today.getDay();
-const hours = document.querySelectorAll(".hours");
-let todaysHours = hours[day];
-todaysHours.setAttribute("id", "today");
+    const today = new Date();
+    let day = today.getDay();
+    const hours = document.querySelectorAll(".hours");
+    let todaysHours = hours[day];
+    todaysHours.setAttribute("id", "today");
 
 // subscribe validation
  
