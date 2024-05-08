a027fc5a0c3d1e5c4798726a06b3430434bbbc57
Janessa Groves <jgroves1@asu.edu>
2024-05-08 02:14:14 -0500

    hero slideshow :)
    finished coding the hero slides with the dots and everything. Need to check for bugs on other devices after commit.
    
    & a lil css clean up :smile:

--- script.js
+++ script.js
@@ -3,17 +3,33 @@
 // hero slideshow
     // acquire elements
     const heroSlides = document.querySelectorAll(".slide");
-    // NEED TO ADD LIL DOTS TO NAVIGATE BTWN IMAGES
+    const heroDots = document.querySelector(".heroDots");
 
     let activeSlide = 0;
+
+    // create the right number of dots
+        for (let i = 0; i < heroSlides.length; ++i) {
+            const newDot = document.createElement("span");
+            newDot.classList.add("heroDot");
+            heroDots.appendChild(newDot);
+        }
         
+        // gather all created dots and style the first one as active
+        const allDots = document.querySelectorAll(".heroDot");
+        allDots[0].classList.add("active");
+
     // functions
         function currSlide(n) {
             heroSlides.forEach(slide => {
                 slide.classList.remove("active");
             });
 
+            allDots.forEach(heroDot => {
+                heroDot.classList.remove("active");
+            })
+
             heroSlides[n].classList.add("active");
+            allDots[n].classList.add("active");
         }
 
         function nextSlide() {
@@ -25,6 +41,13 @@
 
             currSlide(activeSlide);
         }
+
+    // make the dots click-y
+        allDots.forEach((heroDot, i) => {
+            heroDot.addEventListener("click", () => {
+                currSlide(i);
+            });
+        });
 
     // automate it
     setInterval(nextSlide, 5000);
