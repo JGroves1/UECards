5c0098ef6149b64a31d578ca406e0d1c6d3b097e
Janessa Groves <jgroves1@asu.edu>
2024-10-05 07:09:18 -0500

    Events // Scripts // Minor Adjustments
    MAJOR:
    - updated event page introduction copy
    - added events and html for buttons and such
    - expand events functionality @ mobile size!
    - event filtering/sorting functionality ✨
    - recoded the return to top button to be handled by JS
    
    MINOR:
    - uniform margins at the mobile size... could have combined the code, but decided not to
    - nav :visited hover is easier to see now
    - contact submit hover/focus style
    - submit button hover/focus style
    - topbtn fixed so that its in the same place on diff pages (as mentioned above I also changed it to JS, but seperately...)
    
    this was a lot for a single commit trying to focus on the Events page.

--- script.js
+++ script.js
@@ -149,10 +149,201 @@ function subFunction() {
     };
 };
 
+// event expanding for mobile friendliness 
+function expandDetails() {
+    const expBtns = document.querySelectorAll(".eventExpand");
+    
+    // event listener & local function
+    expBtns.forEach(expBtn => {
+        expBtn.addEventListener("click", function() {
+            // find the closest parent listing & sibling info
+            const eventListing = this.closest(".eventListing");
+            const eventInfo = eventListing.querySelector(".eventInfo");
+            const eventDesc = eventListing.querySelector(".eventDesc");
+            
+            // toggle height & arrow direction
+            if (eventInfo.style.height === "auto") {
+                eventInfo.style.height = "";
+                eventDesc.style.opacity = "";
+                this.style.transform = "rotate(0deg)";
+                this.style.transition = "transform 0.5s";
+            } else {
+                eventInfo.style.height = "auto";
+                eventDesc.style.opacity = "100%";
+                this.style.transform = "rotate(180deg)";
+                this.style.transition = "transform 0.5s";
+            }
+        });
+    });
+};
+
+// now the fun part of event filtering by month/selection! 
+function sortEvents() {
+
+    // are there events?
+    const listings = document.querySelectorAll(".eventListing");
+    if(listings.length > 0) {
+        // gather the other elements
+        const monthDropdown = document.querySelector("#monthsDropdown");
+        const yearDropdown = document.querySelector("#yearDropdown");
+        const moreBtn = document.querySelector("#loadMoreBtn");  
+
+        // check today's month & year
+        const date = new Date();
+        let currMonth = date.getMonth() + 1;
+        let currYear = date.getFullYear();
+
+        // default the dropdown menus to this
+        monthDropdown.value = currMonth;
+        yearDropdown.value = currYear;
+
+        // "globally" store the selections
+        var selectedMonth = parseInt(monthDropdown.value);
+        var selectedYear = parseInt(yearDropdown.value);
+
+        // display the selected events!
+        function display() {
+            // first we have to apply the hidden class 
+            listings.forEach((listing) => {
+                listing.classList.add("hiddenEvent");
+            });
+
+            // filtering based on selections
+            let filteredEvents = [];
+            filteredEvents = Array.from(listings).filter((listing) => {
+                const eventYear = parseInt(listing.classList[2]);
+                const eventMonth = parseInt(listing.classList[1]);
+                return eventYear === selectedYear && eventMonth === selectedMonth;
+            });
+
+            // establishing future events
+            const futureEvents = Array.from(listings).filter((listing) => {
+                const eventYear = parseInt(listing.classList[2]);
+                const eventMonth = parseInt(listing.classList[1]);
+                if(eventYear === selectedYear) {
+                    return eventMonth > selectedMonth;
+                }else if(eventYear > selectedYear) {
+                    return true;
+                }else {
+                    return false;
+                };
+            });
+
+            // default poss visible events
+            var visEvents = filteredEvents.concat(futureEvents);
+
+            // but let's only load up to 4 initially
+            filteredEvents.slice(0, 4).forEach((listing) => {
+                listing.classList.remove("hiddenEvent");
+            });
+            
+            // get the number visible
+            let currItem = Math.min(filteredEvents.length, 4);
+
+            // more button reset -- if there are any future events it shows
+            moreBtn.style.display = (futureEvents.length > 0) ? "block" : "none"
+
+            // no message reset
+            noMsg.style.display = "none";
+
+            // now make the more btn function
+            moreBtn.onclick = () => {
+                // determine how many to load --mathing maths
+                let itemsToDisplay;
+                if(currItem === 1) {
+                    itemsToDisplay = 3;
+                }else if(currItem === 2) {
+                    itemsToDisplay = 2;
+                }else {
+                    itemsToDisplay = 4;
+                };
+
+                // show em
+                for (let i = currItem; i < currItem + itemsToDisplay && i < visEvents.length; i++) {
+                    visEvents[i].classList.remove("hiddenEvent");
+                };
+
+                // update number visible
+                currItem += itemsToDisplay;
+
+                // show or hide more button
+                var remainingEvents = visEvents.length - currItem;
+                if (remainingEvents <= 0) {
+                    moreBtn.style.display = "none";
+                };
+
+                // no message reset
+                noMsg.style.display = "none";
+            };
+
+            // no events message show/hide
+            if (currItem === 0) {
+                let noMsg = document.querySelector("#noMsg");
+                noMsg.style.display = "block";
+            };
+        };
+
+        // go go go! -- defaults show
+        display();
+
+        // functions when user selects month/year
+        // display events based on month selection
+        monthDropdown.addEventListener("change", () => {
+            selectedMonth = parseInt(monthDropdown.value);
+    
+            listings.forEach((listing) => {
+                listing.classList.add("hiddenEvent");
+            });
+    
+            display();
+        });
+  
+        // display events based on year selection
+        yearDropdown.addEventListener("change", () => {
+            selectedYear = parseInt(yearDropdown.value);
+    
+            listings.forEach((listing) => {
+                listing.classList.add("hiddenEvent");
+            });
+    
+            display();
+        });
+    };
+};
+
+// Return to Top Button (About & Events Pages)
+function returnToTop() {
+    const topBtn = document.getElementById("topBtn");
+
+    // if it's there, we're gonna make it visible after the user scrolls
+    if(topBtn) {
+        function toggleBtnVis() {
+            if(window.pageYOffset > 100) {
+                topBtn.style.display = "block";
+            }else {
+                topBtn.style.display = "none";
+            };
+        };
+
+        // now make it work
+        function scrollToTop(e) {
+            e.preventDefault();
+            window.scrollTo({top: 0, behavior: "smooth"});
+        };
+
+        // event listeners
+        window.addEventListener("scroll", toggleBtnVis);
+        topBtn.addEventListener("click", scrollToTop);
+    };
+};
+
 // time to call the functions on page load
 document.addEventListener("DOMContentLoaded", function() {
     highlightToday();
     heroSlideshow();
     formFunction();
     subFunction();
-})
+    expandDetails();
+    sortEvents();
+    returnToTop();
+});
